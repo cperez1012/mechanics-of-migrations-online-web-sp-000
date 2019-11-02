932f632fa9a85fc3b8beddf21b8342a4e10b1359
Christian Perez <perezchristian1012@gmail.com>
2019-11-02 15:28:00 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
+    create_table :artists do |t|
+    end
   end
-
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
